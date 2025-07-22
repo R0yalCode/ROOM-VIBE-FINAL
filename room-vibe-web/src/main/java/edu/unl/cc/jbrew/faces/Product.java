970346affdb3762cab533f3e1b55457a1a7c83c0
Royel <royel.jima@unl.edu.ec>
2025-07-22 12:55:35 -0500

    Develop (#6)
    * Add files via upload
    
    * Add files via upload
    
    vista para las habitaciones y sus caracteristicas
    
    * Add files via upload
    
    imagenes para el carrusel de las habitaciones
    
    * Update index.xhtml
    
    vista del carrusel de imagenes para las habitaciones
    
    * Updated team roles in the README.md
    
    ---------
    
    Co-authored-by: Daniel Saavedra <158290540+Dan-San837@users.noreply.github.com>

---
+++ room-vibe-web/src/main/java/edu/unl/cc/jbrew/faces/Product.java
@@ -0,0 +1,49 @@
+package edu.unl.cc.jbrew.faces;
+
+public class Product {
+    
+    private String name;
+    private String image;
+    private double price;
+    private String inventoryStatus;
+
+    public Product(String name, String image, double price, String inventoryStatus) {
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.inventoryStatus = inventoryStatus;
+    }
+
+    // Getters y setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getInventoryStatus() {
+        return inventoryStatus;
+    }
+
+    public void setInventoryStatus(String inventoryStatus) {
+        this.inventoryStatus = inventoryStatus;
+    }
+}
